672d01f4da00f0398a4a81550e1f28322ee23592
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-30 00:58:15 +0100

    Update the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import Home from 'pages/Home';
+import ErrorPage from 'pages/ErrorPage';
 
 const App = () => (
   <>
     <Header />
-    <main className="main">
-      <h1>React Redux Template</h1>
-    </main>
+    <Container fluid as="main" className="main p-0">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Container>
     <Footer />
   </>
 );
