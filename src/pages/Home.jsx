330fd95bb6c86e1870fd5c27a081ae25a096e322
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-30 00:55:45 +0100

    Update home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketOverview from 'components/MarketOverview';
+import ExchangerList from 'components/ExchangerList';
+
+const Home = () => (
+  <>
+    <MarketOverview />
+    <ExchangerList />
+  </>
+);
+
+export default Home;
