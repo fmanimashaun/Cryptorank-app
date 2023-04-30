ba67526e3482833925f2801f011a529aa168cd82
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-04-30 01:00:09 +0100

    Update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from 'features/counter/counterSlice';
-import financialReducer from 'features/financial/finanialSlice';
+import financialReducer from 'features/exchanger/exchangerSlice';
 
 const store = configureStore({
   reducer: {
