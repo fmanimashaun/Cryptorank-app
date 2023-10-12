872c50fa93a916693a072525f1369d9635e41f38
fmanimashaun <fisayo.animashaun@outlook.com>
2023-10-12 04:48:48 +0100

    Update ExchangerDetails component

--- src/pages/ExchangerDetails.jsx
+++ src/pages/ExchangerDetails.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ExchangerDetails = () => {
   const { id } = useParams();
 
   return (
     <div>
-      <h1>Exchanger {id}Details</h1>
+      <h1>
+        Exchanger
+        {' '}
+        {id}
+        Details
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default ExchangerDetails
+export default ExchangerDetails;
