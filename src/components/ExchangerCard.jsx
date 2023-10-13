06975ef8a4a772cc7e584917a391c8bd178d500b
fmanimashaun <fisayo.animashaun@outlook.com>
2023-10-13 18:50:10 +0100

    Update exchanger card

--- src/components/ExchangerCard.jsx
+++ src/components/ExchangerCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 import Style from 'assets/scss/exchangercard.module.scss';
 
-const ExchangerCard = ({ exchanger }) => (
+const ExchangerCard = ({ exchanger, showDetails }) => (
   <Card className={Style.card}>
     <Card.Img
       src={exchanger.image}
@@ -13,7 +13,11 @@ const ExchangerCard = ({ exchanger }) => (
     <Card.Body className={Style.body}>
       <Card.Title>{exchanger.name}</Card.Title>
       <Card.Text className={Style.text}>{`Ranking: ${exchanger.trust_score_rank}`}</Card.Text>
-      <Button variant="primary" className={Style.btn}>
+      <Button
+        variant="primary"
+        className={Style.btn}
+        onClick={() => showDetails(exchanger.id)}
+      >
         See Details
       </Button>
     </Card.Body>
@@ -25,7 +29,9 @@ ExchangerCard.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     trust_score_rank: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
   }).isRequired,
+  showDetails: PropTypes.func.isRequired,
 };
 
 export default ExchangerCard;
