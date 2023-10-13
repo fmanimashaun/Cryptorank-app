d27cdd84440f81bf033b3515143cefc9acba20ff
fmanimashaun <fisayo.animashaun@outlook.com>
2023-10-13 08:36:06 +0100

    Update the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchExchangers } from 'features/exchanger/exchangerSlice';
 import MarketOverview from 'components/MarketOverview';
 import ExchangerList from 'components/ExchangerList';
+import ExchangerListHeader from 'components/ExchangerListHeader';
 
-const Home = () => (
-  <>
-    <MarketOverview />
-    <ExchangerList />
-  </>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchExchangers());
+  }, [dispatch]);
+
+  return (
+    <>
+      <MarketOverview />
+      <ExchangerListHeader />
+      <ExchangerList />
+    </>
+  );
+};
 
 export default Home;
